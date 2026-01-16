2da3309e71165fa75a6f9279b84f90bf2b5f0ef0
ivchenkoIL <gsmhick@gmail.com>
2026-01-17 00:29:11 +0500

    Add redirect from root to tickeAdd root URL redirect to tickets for homepagets URL

--- corp_site/corp_site/urls.py
+++ corp_site/corp_site/urls.py
@@ -3,6 +3,7 @@ URL configuration for corp_site project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
+
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,10 +15,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('tickets/', include('tickets.urls')),
+    path("admin/", admin.site.urls),
+    path("tickets/", include("tickets.urls")),
+    path("", RedirectView.as_view(url="tickets/")),
 ]
